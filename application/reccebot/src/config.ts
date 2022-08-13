e410359ddd6cff74a9033c87a01d2246deb184e7
Ferrois <ferrois2005@gmail.com>
2022-08-14 01:50:37 +0800

    -rectify config.ts to fit production

--- application/reccebot/src/config.ts
+++ application/reccebot/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    // apiURL: 'https://reccebot.herokuapp.com/api',
-    apiURL: 'http://localhost:4000',
+    apiURL: 'https://reccebot.herokuapp.com',
+    // apiURL: 'http://localhost:4000',
 }
 
 export default config;
